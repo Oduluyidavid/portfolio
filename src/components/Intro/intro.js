60445730e5ce056ac06d57e492402dee917f8e03
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2023-11-16 07:45:05 -0800

    today

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,33 +3,37 @@ import "./intro.css";
 import bg from "../../assets/Image D.jpg";
 import btnImg from "../../asset/hireme.png";
 import { Link } from "react-scroll";
+import Container from "react-bootstrap/Container";
 
 const Intro = () => {
   return (
-    <section id="intro">
-      <div className="introContent">
-        <span className="hello">Hello,</span>
-        <span className="introtext">
-          I'm
-          <span className="introname">
-            David <br />
+    <Container>
+      <section id="intro">
+        <div className="introContent">
+          <span className="hello">Hello,</span>
+          <span className="introtext">
+            I'm
+            <span className="introname">
+              David <br />
+            </span>
+            FRONTEND DEVELOPER
           </span>
-          FRONTEND DEVELOPER
-        </span>
-        <p className="intropara">
-          I am a developer like the magician behind the curtains, making the web
-          pages come to life also <br /> responsible for creating the visual
-          elements that users interact with in a web application or website
-        </p>
-        <Link>
-          <button className="btn">
-            <img src={btnImg} alt="Hire" className="btnImg" />
-            <b>Hire Me</b>
-          </button>
-        </Link>
-      </div>
-      <img src={bg} alt="Profile" className="bg" />
-    </section>
+          <p className="intropara">
+            I am a developer like the magician behind the curtains, making the
+            web pages come to life also <br /> responsible for creating the
+            visual elements that users interact with in a web application or
+            website
+          </p>
+          <Link>
+            <button className="btn">
+              <img src={btnImg} alt="Hire" className="btnImg" />
+              <b>Hire Me</b>
+            </button>
+          </Link>
+        </div>
+        <img src={bg} alt="Profile" className="bg" />
+      </section>
+    </Container>
   );
 };
 
--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -6,26 +6,31 @@ import portfolio3 from "../../asset/portfolio-3.png";
 import portfolio4 from "../../asset/portfolio-4.png";
 import portfolio5 from "../../asset/portfolio-5.png";
 import portfolio6 from "../../asset/portfolio-6.png";
+import Container from "react-bootstrap/Container";
 
 const Works = () => {
   return (
-    <section id="works">
-      <h2 className="worksTitle"> My Portfolio</h2>
-      <span className="worksDesc">
-        I take pride in paying attention to details affecting mobile application
-        and website
-      </span>
-      <div className="worksImgs">
-        <img src={portfolio1} alt="" className="worksImg" />
-        <img src={portfolio2} alt="" className="worksImg" />
-        <img src={portfolio3} alt="" className="worksImg" />
-        <img src={portfolio4} alt="" className="worksImg" />
-        <img src={portfolio5} alt="" className="worksImg" />
-        <img src={portfolio6} alt="" className="worksImg" />
-      </div>
-      <button className="workBtn"><b>See More</b></button>
-    </section>
+    <Container>
+      <section id="works">
+        <h2 className="worksTitle"> My Portfolio</h2>
+        <span className="worksDesc">
+          I take pride in paying attention to details affecting mobile
+          application and website
+        </span>
+        <div className="worksImgs">
+          <img src={portfolio1} alt="" className="worksImg" />
+          <img src={portfolio2} alt="" className="worksImg" />
+          <img src={portfolio3} alt="" className="worksImg" />
+          <img src={portfolio4} alt="" className="worksImg" />
+          <img src={portfolio5} alt="" className="worksImg" />
+          <img src={portfolio6} alt="" className="worksImg" />
+        </div>
+        <button className="workBtn">
+          <b>See More</b>
+        </button>
+      </section>
+    </Container>
   );
-}
+};
 
 export default Works;
