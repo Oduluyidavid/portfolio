ec6ea761973d95f882c01cadd10c17e0f7c93e94
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2023-11-16 07:32:56 -0800

    today

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,23 +1,36 @@
 import React from "react";
 import "./navbar.css";
 import logo from "../../assets/Logo.png.jpg";
-import contactImg from '../../assets/contact.png.jpg';
+import contactImg from "../../assets/contact.png.jpg";
 import { Link } from "react-scroll";
+import Container from "react-bootstrap/Container";
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <img src={logo} alt={logo} className="logo" />
-      <div className="desktopMenu">
-        <Link className="desktopMenuListItem"><b>Home</b></Link>
-        <Link className="desktopMenuListItem"><b>About</b></Link>
-        <Link className="desktopMenuListItem"><b>Portfolio</b></Link>
-        <Link className="desktopMenuListItem"><b>Clients</b></Link>
-      </div>
+    <Container>
+      <nav className="navbar">
+        <img src={logo} alt={logo} className="logo" />
+        <div className="desktopMenu">
+          <Link className="desktopMenuListItem">
+            <b>Home</b>
+          </Link>
+          <Link className="desktopMenuListItem">
+            <b>About</b>
+          </Link>
+          <Link className="desktopMenuListItem">
+            <b>Portfolio</b>
+          </Link>
+          <Link className="desktopMenuListItem">
+            <b>Clients</b>
+          </Link>
+        </div>
 
-      <button className="desktopMenuBtn">
-        <img src={contactImg} alt="" className="desktopMenuImg" /><b>Contact Me</b></button>
-    </nav>
+        <button className="desktopMenuBtn">
+          <img src={contactImg} alt="" className="desktopMenuImg" />
+          <b>Contact Me</b>
+        </button>
+      </nav>
+    </Container>
   );
 };
 
